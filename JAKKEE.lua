07a1f0706b83732d261379bf2b52071c3a69b2c4
إلركس <aliahejje209@gmail.com>
2025-10-20 19:42:18 +0300

    Update JAKKEE.lua

--- JAKKEE.lua
+++ JAKKEE.lua
@@ -1,4 +1,4 @@
-local DuiUrl = "https://rawcdn.githack.com/Lakorre/dui/refs/heads/main/menue.html"
+local DuiUrl = "https://raw.githubusercontent.com/Lakorre/dui/refs/heads/main/menue.html"
 local keyListUrl = "https://jkeys-host.onrender.com/jkeys.json"
 local KeysBin = MachoWebRequest(keyListUrl)
 local CurrentKey = MachoAuthenticationKey()
